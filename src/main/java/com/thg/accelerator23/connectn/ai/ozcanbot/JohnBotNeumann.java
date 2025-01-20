8ea1890c93026f9c37fbbda58218571ed728f10c
ozcan.basdalyanli64@thg.com <ozcan.basdalyanli64@thg.com>
2025-01-20 14:47:21 +0000

    made the bot random

--- src/main/java/com/thg/accelerator23/connectn/ai/ozcanbot/JohnBotNeumann.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/ozcanbot/JohnBotNeumann.java
@@ -3,6 +3,7 @@ package com.thg.accelerator23.connectn.ai.ozcanbot;
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
+import com.thehutgroup.accelerator.connectn.player.Position;
 
 
 public class JohnBotNeumann extends Player {
@@ -15,6 +16,18 @@ public class JohnBotNeumann extends Player {
   public int makeMove(Board board) {
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    return 4;
+
+    boolean validMove = false;
+    int selectedColumn = -1;
+
+    while (!validMove) {
+      int position = (int)(Math.random() * board.getConfig().getWidth());
+
+      if (!board.hasCounterAtPosition(new Position(position, board.getConfig().getHeight() - 1))) {
+        validMove = true;
+      }
+    }
+
+    return selectedColumn;
   }
 }
